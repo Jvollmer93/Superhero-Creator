7786c7ba42a1334a46583e2635a333296852f0fb
John Vollmer <vollme22@uwm.edu>
2018-07-02 09:21:38 -0500

    initial commit

---
+++ SuperheroCreator/Models/Superhero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroCreator.Models
+{
+    public class Superhero
+    {
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
